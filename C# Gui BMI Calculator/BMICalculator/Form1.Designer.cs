fbce8ad17f729014d0d10af786d718ea8fb6da7d
MatanBarhoum <88832406+MatanBarhoum@users.noreply.github.com>
2022-05-23 22:52:59 +0300

    Add files via upload

---
+++ C# Gui BMI Calculator/BMICalculator/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace BMICalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.heightValue = new System.Windows.Forms.TextBox();
+            this.weightValue = new System.Windows.Forms.TextBox();
+            this.ageValue = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.resultValue = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Height";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Weight";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Age";
+            // 
+            // heightValue
+            // 
+            this.heightValue.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.heightValue.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heightValue.Location = new System.Drawing.Point(111, 65);
+            this.heightValue.Name = "heightValue";
+            this.heightValue.Size = new System.Drawing.Size(100, 25);
+            this.heightValue.TabIndex = 3;
+            // 
+            // weightValue
+            // 
+            this.weightValue.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.weightValue.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weightValue.Location = new System.Drawing.Point(114, 113);
+            this.weightValue.Name = "weightValue";
+            this.weightValue.Size = new System.Drawing.Size(100, 25);
+            this.weightValue.TabIndex = 4;
+            // 
+            // ageValue
+            // 
+            this.ageValue.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ageValue.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ageValue.Location = new System.Drawing.Point(114, 162);
+            this.ageValue.Name = "ageValue";
+            this.ageValue.Size = new System.Drawing.Size(100, 25);
+            this.ageValue.TabIndex = 5;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.Location = new System.Drawing.Point(88, 207);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(89, 31);
+            this.submitButton.TabIndex = 6;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(22, 266);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(76, 25);
+            this.resultLabel.TabIndex = 7;
+            this.resultLabel.Text = "Result: ";
+            // 
+            // resultValue
+            // 
+            this.resultValue.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultValue.Location = new System.Drawing.Point(104, 266);
+            this.resultValue.Name = "resultValue";
+            this.resultValue.Size = new System.Drawing.Size(100, 25);
+            this.resultValue.TabIndex = 8;
+            this.resultValue.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(52, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "BMI Calculator";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(214, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "m";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(214, 113);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "kg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(264, 319);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.resultValue);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.ageValue);
+            this.Controls.Add(this.weightValue);
+            this.Controls.Add(this.heightValue);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BMI Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox heightValue;
+        private System.Windows.Forms.TextBox weightValue;
+        private System.Windows.Forms.TextBox ageValue;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.RichTextBox resultValue;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ C# Gui BMI Calculator/BMICalculator/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMICalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public static double result { get; set; }
+        public static double roundedResult { get; set; }
+
+        public static bool notpassed = true;
+        public void submitButton_Click(object sender, EventArgs e)
+        {
+            CheckRequirements();
+            if (notpassed)
+            {
+                return;
+            }
+            else
+            {
+                double mass = Convert.ToDouble(weightValue.Text);
+                double height = Convert.ToDouble(heightValue.Text);
+                result = mass / (height * height);
+                roundedResult = Math.Round(result, 1);
+
+                if (roundedResult <= 16.0)
+                {
+                    resultValue.ForeColor = Color.Red;
+                    resultValue.Text = "Severe thinness";
+                }
+                else if (roundedResult > 16.0 && roundedResult <= 16.9)
+                {
+                    resultValue.ForeColor = Color.Red;
+                    resultValue.Text = "Moderate thinness";
+                }
+                else if (roundedResult >= 17.0 && roundedResult <= 18.4)
+                {
+                    resultValue.ForeColor = Color.Red;
+                    resultValue.Text = "Mild thinness";
+                }
+                else if (roundedResult >= 18.5 && roundedResult <= 24.9)
+                {
+                    resultValue.ForeColor = Color.Green;
+                    resultValue.Text = "Normal";
+                }
+                else if (roundedResult >= 25.0 && roundedResult <= 29.9)
+                {
+                    resultValue.ForeColor = Color.Orange;
+                    resultValue.Text = "Overweight";
+                }
+                else if (roundedResult > 30.0)
+                {
+                    resultValue.ForeColor = Color.Red;
+                    resultValue.Text = "Obese";
+                }
+            }
+        }
+
+        public void CheckRequirements()
+        {
+            if (string.IsNullOrEmpty(heightValue.Text) ||
+                string.IsNullOrEmpty(weightValue.Text) ||
+                string.IsNullOrEmpty(ageValue.Text))
+            {
+                MessageBox.Show("One or more rows are empty.", "Warning!");
+            }
+            else
+            {
+                notpassed = false;
+            }
+        }
+    }
+}
+
